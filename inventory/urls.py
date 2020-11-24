0995016de2137b9b31b24de115723a47a2339eac
ecarrillo99 <carrilloedisson@gmail.com>
2020-11-23 21:30:05 -0500

    commit

--- inventory/urls.py
+++ inventory/urls.py
@@ -18,9 +18,16 @@ from django.urls import path, include
 
 from .views import dashboard
 
+from django.views.static import serve
+from django.conf.urls import url
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', dashboard, name='dashboard'),
     path('users/', include('users.urls')),
     path('store/', include('store.urls')),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
